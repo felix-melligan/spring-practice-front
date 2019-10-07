0157f3e11f6c6d17f1ac030d5fe12c944c44a51b
Felix Melligan <felixmelligan@hotmail.co.uk>
2019-10-07 15:57:41 -0400

    First push

--- src/App.js
+++ src/App.js
@@ -2,25 +2,56 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: []
+    }
+    this.request();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <span className="Data">
+          {this.displayData(this.state.data)}
+        </span>
+      </div>
+    );
+  }
+
+  displayData(data) {
+    let returnArray = [];
+    data.forEach(element => {
+      returnArray.push(
+        <span key={element.id}>
+          <h1>{element.first_name+" "+element.last_name}</h1>
+            <ul>
+              <li>
+                {"id: "+element.id}
+                </li>
+              <li>
+                {"Gender: "+element.gender}
+              </li>
+              <li>
+                {"Email: "+element.email}
+              </li>
+              <li>
+                {"IP: "+element.ip_address}
+              </li>
+            </ul>
+        </span>
+      );
+    });
+    return returnArray;
+  }
+
+  async request() {
+    const response = await fetch('http://localhost:8080/users');
+    this.setState({data: await response.json()});
+  }
 }
 
 export default App;
